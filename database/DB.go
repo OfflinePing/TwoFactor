b7fda0f94640a1350cd4d943cf288511bd4c0402
OfflinePing <pumperniccl@gmail.com>
2023-07-16 02:01:56 +0200

    This is a 2FA Dashboard written in GoLang
    it encrypts the secret before saving or loading.
    
    if you find any bugs or vulnabilities open a Github issue!

---
+++ database/DB.go
@@ -0,0 +1,262 @@
+package database
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"github.com/pquerna/otp/totp"
+	"io"
+	_ "modernc.org/sqlite"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var DB *sql.DB
+var err error
+
+type User struct {
+	User string
+	Pass string
+	MFA  []Faktor
+}
+
+type Faktor struct {
+	Name   string
+	Secret string
+}
+
+var Faktors []User
+
+func Encrypt(passphrase, text string) (string, error) {
+	// Create a new AES-256 cipher with the SHA-256 of the passphrase
+	hasher := sha256.New()
+	hasher.Write([]byte(passphrase))
+	block, err := aes.NewCipher(hasher.Sum(nil))
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(text))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(text))
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+func Decrypt(passphrase, cipherText string) (string, error) {
+	// Create a new AES-256 cipher with the SHA-256 of the passphrase
+	hasher := sha256.New()
+	hasher.Write([]byte(passphrase))
+	block, err := aes.NewCipher(hasher.Sum(nil))
+	if err != nil {
+		return "", err
+	}
+
+	decodedCipherText, err := base64.URLEncoding.DecodeString(cipherText)
+	if err != nil {
+		return "", err
+	}
+
+	if len(decodedCipherText) < aes.BlockSize {
+		return "", errors.New("ciphertext block size is too short")
+	}
+
+	iv := decodedCipherText[:aes.BlockSize]
+	decodedCipherText = decodedCipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(decodedCipherText, decodedCipherText)
+
+	return string(decodedCipherText), nil
+}
+
+func HashPassword(password string) string {
+	h := sha256.New()
+	h.Write([]byte(password))
+	return string(h.Sum(nil))
+}
+
+func GetUserMFA(username string) []Faktor {
+	for _, f := range Faktors {
+		if f.User == username {
+			return f.MFA
+		}
+	}
+	return []Faktor{}
+}
+
+func GetFACode(pasword string, secret string) string {
+	sec, err := Decrypt(pasword, secret)
+	if err != nil {
+		return ""
+	}
+	code, err := totp.GenerateCode(sec, time.Now())
+	if err != nil {
+		return ""
+	}
+	return code
+}
+func Connect() {
+	DB, err = sql.Open("sqlite", "./database.db")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Close() {
+	DB.Close()
+}
+
+func CreateTables() {
+	_, err := DB.Exec("CREATE TABLE IF NOT EXISTS faktor (user TEXT, password TEXT, mfa BLOB)")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AddUser(username, password string) {
+	faktors := []Faktor{}
+	faktorsJson, err := json.Marshal(faktors)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = DB.Exec("INSERT INTO faktor VALUES (?, ?, ?)", username, HashPassword(password), faktorsJson)
+	if err != nil {
+		panic(err)
+	}
+	Faktors = append(Faktors, User{username, password, []Faktor{}})
+}
+
+func AddFaktor(name string, password string, secret string, username string) {
+	faktors := GetUserMFA(username)
+	encrypt, err := Encrypt(password, secret)
+	if err != nil {
+		panic(err)
+	}
+	faktors = append(faktors, Faktor{Name: name, Secret: encrypt})
+	faktorsJson, err := json.Marshal(faktors)
+	if err != nil {
+		panic(err)
+	}
+	_, err = DB.Exec("UPDATE faktor SET mfa=? WHERE user=?", faktorsJson, username)
+	if err != nil {
+		panic(err)
+	}
+	Load()
+}
+
+func Load() {
+	rows, err := DB.Query("SELECT * FROM faktor")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	Faktors = []User{}
+	for rows.Next() {
+		var username string
+		var password string
+		var mfaJSON []byte
+
+		err = rows.Scan(&username, &password, &mfaJSON)
+		if err != nil {
+			panic(err)
+		}
+
+		var mfa []Faktor
+		err = json.Unmarshal(mfaJSON, &mfa)
+		if err != nil {
+			panic(err)
+		}
+
+		Faktors = append(Faktors, User{username, password, mfa})
+	}
+
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+}
+
+func DeleteFaktor(name string, username string) {
+	faktor := RemoveFaktor(name, username)
+	faktorJson, err := json.Marshal(faktor)
+	_, err = DB.Exec("UPDATE faktor SET mfa=? WHERE user=?", faktorJson, username)
+	if err != nil {
+		panic(err)
+	}
+	for i, f := range Faktors {
+		if f.User == username {
+			Faktors[i].MFA = RemoveFaktor(name, username)
+			break
+		}
+	}
+}
+
+func RemoveFaktor(name string, username string) []Faktor {
+	var mfa []Faktor
+	for _, f := range GetUserMFA(username) {
+		if f.Name != name {
+			mfa = append(mfa, f)
+		}
+	}
+	return mfa
+}
+
+func Authenticate(username string, key string) bool {
+	for _, user := range Faktors {
+		if user.Pass == HashPassword(key) && user.User == username {
+			return true
+		}
+	}
+	return false
+}
+
+func Auth(w http.ResponseWriter, r *http.Request) bool {
+	cookie, err := r.Cookie("session")
+	if err != nil {
+		return false
+	}
+	username := strings.Split(cookie.Value, " ")[0]
+	password := strings.Split(cookie.Value, " ")[1]
+	if Authenticate(username, password) {
+		return true
+	}
+	return false
+}
+
+func CheckChars(text string) bool {
+	chars := []string{";", " ", "'", "&", "*", "|", "<", ">", "$", "\\", "/", "@", "%", "."}
+	for _, v := range chars {
+		if strings.Contains(text, v) {
+			return true
+		}
+	}
+	return false
+}
+
+func GetFile(name string) string {
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	b := make([]byte, 100000)
+	n, err := file.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return string(b[:n])
+}
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"TwoFaktor/database"
+	"fmt"
+	"golang.org/x/net/websocket"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWSOrder(ws *websocket.Conn) {
+	fmt.Println("New Orderbook Client Connection: ", ws.RemoteAddr())
+
+	user, err := ws.Request().Cookie("session")
+	if err != nil {
+		fmt.Println("No cookie found")
+		return
+	}
+
+	username := strings.Split(user.Value, " ")[0]
+	password := strings.Split(user.Value, " ")[1]
+
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			n, err := ws.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading message: ", err.Error())
+				return
+			}
+			msg := string(buf[:n])
+			if strings.Contains(msg, "ADD> ") {
+				msg = strings.Replace(msg, "ADD> ", "", 1)
+				database.AddFaktor(strings.Split(msg, ":")[0], password, strings.Split(msg, ":")[1], username)
+			}
+			if strings.Contains(msg, "DELETE> ") {
+				msg = strings.Replace(msg, "DELETE> ", "", 1)
+				database.DeleteFaktor(strings.Split(msg, ":")[0], username)
+			}
+		}
+	}()
+
+	for {
+		var msg string
+		for _, mfa := range database.GetUserMFA(username) {
+			code := database.GetFACode(password, mfa.Secret)
+			msg += mfa.Name + ":" + code + ","
+		}
+		if len(msg) > 0 {
+			msg = strings.TrimSuffix(msg, ",")
+			err := websocket.Message.Send(ws, msg)
+			if err != nil {
+				fmt.Println("Error sending message: ", err.Error())
+
+				return
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func SetSession(username string, key string, w http.ResponseWriter) {
+	expiration := time.Now().Add(6 * time.Hour)
+	cookie := http.Cookie{Name: "session", Value: username + " " + key, Expires: expiration}
+	http.SetCookie(w, &cookie)
+}
+
+func RegisterPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		fmt.Println(username, password)
+		if len(username) == 0 || len(password) == 0 {
+			fmt.Println("Username or password is empty")
+			return
+		}
+		if database.CheckChars(username) || database.CheckChars(password) {
+			fmt.Println("Username or password contains invalid characters")
+			return
+		}
+		database.AddUser(username, password)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(database.GetFile("web/register.html")))
+}
+
+func LoginPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		if len(username) == 0 || len(password) == 0 {
+			fmt.Println("Username or password is empty")
+			return
+		}
+		if database.Authenticate(username, password) {
+			SetSession(username, password, w)
+			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(database.GetFile("web/index.html")))
+}
+
+func DashboardPage(w http.ResponseWriter, r *http.Request) {
+	if !database.Auth(w, r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+
+	ses, err := r.Cookie("session")
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	name := strings.Split(ses.Value, " ")[0]
+	dash := strings.NewReplacer("{{name}}", name).Replace(database.GetFile("web/dashboard.html"))
+	w.Write([]byte(dash))
+}
+
+func main() {
+	database.Connect()
+	fmt.Println("Connected to database")
+	database.CreateTables()
+	fmt.Println("Created tables")
+	database.Load()
+	fmt.Println("Loaded data")
+	defer database.Close()
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWSOrder))
+	http.HandleFunc("/", LoginPage)
+	http.HandleFunc("/dashboard", DashboardPage)
+	http.HandleFunc("/register", RegisterPage)
+	http.ListenAndServe(":8080", nil)
+}
